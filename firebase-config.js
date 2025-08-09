bdeac3f14a86bfcd854c1eb42ef447b582f79857
Padenn25 <padwottmisoft@protonmail.com>
2025-08-09 21:44:37 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+window.__firebase_config = JSON.stringify({
+  apiKey: "DEIN_API_KEY",
+  authDomain: "DEIN_AUTH_DOMAIN",
+  projectId: "DEIN_PROJECT_ID",
+  storageBucket: "DEIN_STORAGE_BUCKET",
+  messagingSenderId: "DEINE_MESSAGING_SENDER_ID",
+  appId: "DEINE_APP_ID"
+});
+
+window.__app_id = "pads-todo";
